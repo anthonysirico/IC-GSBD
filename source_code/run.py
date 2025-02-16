ce7b011643116907a863dacbe6b247e03dad614e
Anthony Sirico <100864754+anthonysirico@users.noreply.github.com>
2025-02-16 16:11:56 -0600

    Update run.py

--- source_code/run.py
+++ source_code/run.py
@@ -1,8 +1,6 @@
 import iteration as it
 from model import GCN
-import torch
 from torch_geometric.loader import DataLoader
-import torch
 import time
 from tqdm import tqdm
 import numpy as np
@@ -10,11 +8,8 @@ import pandas as pd
 from scipy.io import loadmat
 import pandas as pd
 import torch
-import numpy as np
-from tqdm import tqdm
 from torch_geometric.utils import from_networkx
 import os
-from tqdm import tqdm
 import random
 
 def train():
